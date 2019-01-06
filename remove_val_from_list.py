6501518500f549be80ae2a8d2dbe222ee4f36123
Vidul Ayakulangara Panickan <apvidul@gmail.com>
2019-01-06 14:15:00 -0500

    Update remove_val_from_list.py

--- remove_val_from_list.py
+++ remove_val_from_list.py
@@ -15,3 +15,17 @@ class Solution(object):
                     if j== len(nums)-1:
                         return i
                         
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = 0 
+        for j in range(len(nums)):
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
+        return i
